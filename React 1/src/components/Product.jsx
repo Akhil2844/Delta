f2725d1c9db9cfc7cdfd187f8d492fa7b36b03c6
Akhil2844 <21bch075@nith.ac.in>
2024-03-14 17:01:14 +0530

    first commit

---
+++ React 1/src/components/Product.jsx
@@ -0,0 +1,12 @@
+import "./Product.css"
+function Product()
+{
+    return(
+        <div className="Product">
+            <p>Product Title</p>
+            <p>Product Description</p>
+        </div>
+    )
+}
+
+export default Product
