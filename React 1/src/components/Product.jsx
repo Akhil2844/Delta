7084a5dc20c752f195039347c5a6f08d20f99c17
Akhil2844 <21bch075@nith.ac.in>
2024-03-23 16:36:41 +0530

    add

--- React 1/src/components/Product.jsx
+++ React 1/src/components/Product.jsx
@@ -1,17 +1,14 @@
-// Product.jsx
 import React from "react";
 import "./Product.css";
-import { Linter } from "eslint";
 
-function Product({ title, price, features, features2 }) {
-  // const list=features.map((feature)=> <li>{feature}</li>)
+function Product({ title, price, features }) {
+  const list=features.map((feature,index)=> <li>{feature}</li>);
   return (
     <div className="Product">
       <p>{title}</p>
       <p>Price: {price}</p>
-    
-
-
+   
+<p>{list}</p>
     </div>
   );
 }
--- React 1/src/components/ProductTab.jsx
+++ React 1/src/components/ProductTab.jsx
@@ -3,12 +3,12 @@ import Product from "./Product.jsx"
 function ProductTab()
 {
     let options=["hitech","durable","fast"];
-    // let options=[<li>hi-tech</li>,<li>durable</li>,<li>fast</li>]
+    // let options=[<li>hi-tech</li>,<li>durable</li>,<li>fast</li>];
     // let options2={a:"hitech",b: "durab",c:"akhil"};
   
     return(
         <div>
-            <Product title="Laptop" price="60,000"  features={options} features2={{a:"hitech",b: "durab",c:"akhil"}} />
+            <Product title="Laptop" price="60,000"  features={options} /*features2={{a:"hitech",b: "durab",c:"akhil"}}*/ />
             {/* <Product title="Phone" price="20,000"  />
             <Product title="Pen" price={100}/> */}
 
